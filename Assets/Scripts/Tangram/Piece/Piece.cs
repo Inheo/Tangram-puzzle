0cd67663b5acca8c8df570f651f0cf4d5a77b831
Inheo <agch777505@gmail.com>
2022-05-11 19:41:28 +0300

    added mehanic wrong pin piece

--- Assets/Scripts/Tangram/Piece/Piece.cs
+++ Assets/Scripts/Tangram/Piece/Piece.cs
@@ -1,9 +1,13 @@
+using System.Collections;
 using Assets.Interfaces.Events.Emitters;
 using UnityEngine;
 
 public class Piece : MonoBehaviour
 {
     [SerializeField] private float _pinDistance = 2;
+    [SerializeField] private PieceMover _pieceMover;
+
+    private bool _canCollision = false;
 
     private IDragAndDropEventEmitter _dragAndDropEventEmitter;
     private RightPinPointPiece _rightPoint;
@@ -28,6 +32,15 @@ public class Piece : MonoBehaviour
         _dragAndDropEventEmitter.OnDrop -= Drop;
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if(_canCollision && other.attachedRigidbody.TryGetComponent(out Piece piece))
+        {
+            _pieceMover.MoveToShufflePoint();
+            _rightPoint.UnpinPieceOfTangram();
+        }
+    }
+
     private void PickUp()
     {
         _rightPoint.UnpinPieceOfTangram();
@@ -39,11 +52,24 @@ public class Piece : MonoBehaviour
         {
             Pin();
         }
+
+        StartCoroutine(EnableCanCollision());
     }
 
     private void Pin()
     {
         _rightPoint.PinPieceOfTangram(ID);
         _transform.position = _rightPoint.Position;
+    }
+
+    private IEnumerator EnableCanCollision()
+    {
+        _canCollision = true;
+
+        yield return null;
+        yield return null;
+        yield return null;
+
+        _canCollision = false;
     }
 }
