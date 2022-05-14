9210480c5a1af56468be4cf49c8f8164d131e8a2
Inheo <agch777505@gmail.com>
2022-05-14 14:54:14 +0300

    added player game data

---
+++ Assets/Scripts/Data/WorldData.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace Scripts.Data
+{
+    [Serializable]
+    public class WorldData
+    {
+        public int Level = 0;
+
+        public WorldData()
+        {
+            Level = 0;
+        }
+
+        public void CompleteLevel()
+        {
+            Level++;
+        }
+    }
+}
