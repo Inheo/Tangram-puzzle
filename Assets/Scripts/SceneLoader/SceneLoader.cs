0c3f6d669aef20e13fc0b006907e9253b156d302
Inheo <agch777505@gmail.com>
2022-05-14 15:16:59 +0300

    added scene loader

---
+++ Assets/Scripts/SceneLoader/SceneLoader.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader
+{
+    private string _currentScene;
+
+    public event Action SceneLoaded;
+
+    private  MonoBehaviour _coroutineStarter;
+
+    public SceneLoader(MonoBehaviour coroutineStarter)
+    {
+        _coroutineStarter = coroutineStarter;
+    }
+
+    public bool TryLoadLevel(string newScene)
+    {
+        if (!string.IsNullOrEmpty(_currentScene))
+        {
+            SceneManager.UnloadSceneAsync(_currentScene, UnloadSceneOptions.UnloadAllEmbeddedSceneObjects);
+        }
+
+        _currentScene = newScene;
+        _coroutineStarter.StartCoroutine(LoadScene(_currentScene));
+        return true;
+    }
+
+    private IEnumerator LoadScene(string sceneName)
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Additive);
+        yield return null;
+
+        SceneManager.SetActiveScene(SceneManager.GetSceneByName(sceneName));
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+        SceneLoaded?.Invoke();
+    }
+}
