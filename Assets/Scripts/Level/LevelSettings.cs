d9bdbd87eb5d361dfe81a27618f1ae8de7db4486
Inheo <agch777505@gmail.com>
2022-05-14 14:47:43 +0300

    added Level Settings

---
+++ Assets/Scripts/Level/LevelSettings.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class LevelSettings : ScriptableObject
+{
+    [SerializeField] private LevelSettingsItem[] _levelSettings;
+
+    public LevelSettingsItem this[int index]
+    {
+        get
+        {
+            index = index >= _levelSettings.Length ? _levelSettings.Length - 1 : index;
+            return _levelSettings[index];
+        }
+    }
+}
