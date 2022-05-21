fc74f20b1b89fc292cce02e95ad1c9f9e6ec1f53
Inheo <agch777505@gmail.com>
2022-05-21 18:03:38 +0300

    added fade panel component

---
+++ Assets/Scripts/UI/FadePanel.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using DG.Tweening;
+
+[RequireComponent(typeof(CanvasGroup))]
+public class FadePanel : MonoBehaviour
+{
+    [SerializeField] private float _animationDuration = 0.2f;
+
+    private CanvasGroup _canvasGroup;
+
+    private void Awake()
+    {
+        _canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    public void Show()
+    {
+        _canvasGroup.DOFade(1, _animationDuration);
+    }
+
+    public void Hide()
+    {
+        _canvasGroup.DOFade(0, _animationDuration);
+    }
+}
